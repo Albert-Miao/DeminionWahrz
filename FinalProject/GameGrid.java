8dc4cb8be4d8f3a440e0f5edf25b359ca8d15eac
Nathaniel Getachew <natugetachew@gmail.com>
2017-05-15 12:11:38 -0700

    I changed the tile class so that each tile is represented by a square, and I began modifying the GameGrid class so that it saves all these tiles in an array called grid(not finished yet).

--- FinalProject/GameGrid.java
+++ FinalProject/GameGrid.java
@@ -5,44 +5,55 @@ import javax.swing.ImageIcon;
 
 public class GameGrid extends JComponent
   {
-  
+  	public Tile[][] grid;
+  	public GameGrid()
+  	{
+  		grid = new Tile[20][20];	
+  	}
     public void paint(Graphics g)
     {
       Graphics2D graph2 = (Graphics2D)g;
       graph2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
       
       //Draw Grid Background
-      drawGrid(graph2,"GridBackground.png");
-      //Populates grid with icons
-	  for(int k = 0; k < 600; k+= 50)
-	  {
-	  	for(int b = 0; b < 600; b+= 50)
-	  	{
-	  	  java.net.URL imgUrl = getClass().getResource("TestIcon.png");
-	  	  ImageIcon icon = new ImageIcon(imgUrl);
-	  	  icon.paintIcon(this,g,k,b);
-	  	}
-	  }
-
+      
+      /*Populates grid with icons
+	  *for(int k = 0; k < 600; k+= 50)
+	  *{
+	  	*for(int b = 0; b < 600; b+= 50)
+	  	*{
+	  	  *java.net.URL imgUrl = getClass().getResource("TestIcon.png");
+	  	  *ImageIcon icon = new ImageIcon(imgUrl);
+	  	 * icon.paintIcon(this,g,k,b);
+	  	*}
+	  *}
+		*/
+		drawGrid(graph2);
     }
-    private void drawGrid(Graphics2D g,String backgroundName)
+    private void drawGrid(Graphics2D g)
     {
-      java.net.URL imgUrl1 = getClass().getResource(backgroundName);
-  	  ImageIcon gridBackground = new ImageIcon(imgUrl1);
-  	  gridBackground.paintIcon(this,g,0,0);
+      /*java.net.URL imgUrl1 = getClass().getResource(backgroundName);
+  	  *ImageIcon gridBackground = new ImageIcon(imgUrl1);
+  	  *gridBackground.paintIcon(this,g,0,0);
+  	  */
+  	  for(int i = 0; i < grid.length; i++)//Fill grid array with null tiles
+  	  {
+  	  	for(int k = 0; k < grid[0].length;k++)
+  	  	{
+  	  		Tile t = null;
+  	  		grid[i][k] = t;
+  	  	}
+  	  }
+  	  int x,y = 0;
+  	  for(int i = 0; i < grid.length; i++)
+  	  {
+  	  	for(int k = 0; k < grid[0].length;k++)
+  	  	{
+  	  		grid[i][k] = new Tile()
+  	  	}
+  	  }
   	  
-      for(int i = 0; i <= 600; i+= 50)
-      {
-	      Shape line = new Line2D.Float(0,i,600,i);
-	      g.setPaint(Color.BLACK);
-	      g.draw(line);
-      }
-      for(int i = 0; i <= 600; i+= 50)
-      {
-	      Shape line = new Line2D.Float(i,0,i,600);
-	      g.setPaint(Color.BLACK);
-	      g.draw(line);
-      }
     }
+    
   }
   
--- FinalProject/Tile.java
+++ FinalProject/Tile.java
@@ -1,3 +1,7 @@
+import java.awt.geom.*;
+import java.awt.*;
+import javax.swing.JComponent;
+import javax.swing.ImageIcon;
 
 public class Tile
 {
@@ -11,8 +15,21 @@ public class Tile
 		xPos = x;
 		yPos = y;
 		terrainType = terrain;
-		
-		
 	}
-	//HI
+	public void makeTile(Graphics2D graphic)
+	{
+		    Shape line1 = new Line2D.Float(xPos,yPos,xPos,yPos + 50);
+	        graphic.setPaint(Color.BLACK);
+	        Shape line2 = new Line2D.Float(xPos,yPos,xPos + 50,yPos);
+	        graphic.setPaint(Color.BLACK);
+	        Shape line3 = new Line2D.Float(xPos,yPos+50,xPos+50,yPos+50);
+	        graphic.setPaint(Color.BLACK);
+	        Shape line4 = new Line2D.Float(xPos+50,yPos,xPos+50,yPos+50);
+	        graphic.setPaint(Color.BLACK);
+	        
+	        graphic.draw(line1);
+	        graphic.draw(line2);
+	        graphic.draw(line3);
+	        graphic.draw(line4);
+	}	
 }
