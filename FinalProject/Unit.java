7543c4b0ea4a1858dec1b1e1e89fe06003ac8e61
Albert Miao <amiao541769@gmail.com>
2017-05-15 12:01:34 -0700

    moved variables img health name to element from unit

--- FinalProject/Unit.java
+++ FinalProject/Unit.java
@@ -1,48 +1,34 @@
 import java.awt.Graphics;
 import javax.swing.ImageIcon;
 
+import info.gridworld.grid.Location;
+
 public abstract class Unit //extends Element
 {
 	 private int attack;
-	 private int defense;
-	 private int range;
-	 private String name;
-	 private String imageName;
-	 private ImageIcon img;
+	 private int movement;
+	 private int rangeMin;
+	 private int rangeMax;
 	  
-	 public Unit(int att, int def, int rnge, String unitName, String imgName)
+	 public Unit(int att, int rngeMin, int rngeMax, int mvement)
 	 {
 		attack = att;
-		defense = def;
-		range = rnge;
-		name = unitName;
-		imageName = imgName;
-		img = new ImageIcon(imgName);
+		rangeMin = rngeMin;
+		rangeMax = rngeMax;
+		movement = mvement;
 	 }
 	 
 	 public int getAttack()
 	 {
 		 return attack;
 	 }
-	 public int getDefense()
-	 {
-		 return defense;
+	 
+	 public Location[] getMovable(){
+	 	//to be implemented
 	 }
-	 public int getRange()
-	 {
-		 return range;
-	 }
-	 public String getName()
-	 {
-		 return name;
-	 }
-	 public ImageIcon getImage()
-	 {
-		 return img;
+	 public Location[] getRange(){
+	 	//to be implemented
 	 }
 	 public abstract void move();
 	 public abstract void attack();
-	 public abstract int getHealth();
-	 public abstract void setHealth();
-	 
 }
