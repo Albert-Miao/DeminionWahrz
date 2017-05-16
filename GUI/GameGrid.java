9282b9ca5f08399fb4b0fd3320a2c408d53c98a9
Nathaniel Getachew <natugetachew@gmail.com>
2017-05-16 12:00:01 -0700

    I re-arranged some of the methods so that it is more user friendly. I also finished the grid array so that it would be easier to work with in the future.

--- GUI/GameGrid.java
+++ GUI/GameGrid.java
@@ -8,31 +8,18 @@ public class GameGrid extends JComponent
   	public Tile[][] grid;
   	public GameGrid()
   	{
-  		grid = new Tile[20][20];	
+  		grid = new Tile[20][20];
+  		populateGridArray();
   	}
-    public void paint(Graphics g)
-    {
-      Graphics2D graph2 = (Graphics2D)g;
-      graph2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-      
-      //Draw Grid Background
-      
-      /*Populates grid with icons
-	  *for(int k = 0; k < 600; k+= 50)
-	  *{
-	  	*for(int b = 0; b < 600; b+= 50)
-	  	*{
-	  	  *java.net.URL imgUrl = getClass().getResource("TestIcon.png");
-	  	  *ImageIcon icon = new ImageIcon(imgUrl);
-	  	 * icon.paintIcon(this,g,k,b);
-	  	*}
-	  *}
-		*/
-		drawGrid(graph2);
-    }
-    private void drawGrid(Graphics2D g)
-    {
-      /*java.net.URL imgUrl1 = getClass().getResource(backgroundName);
+  	public void paint(Graphics g)
+  	{
+  	  Graphics2D graphic = (Graphics2D)g;
+      graphic.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      makeGrid(graphic);
+  	}
+  	private void populateGridArray()
+  	{
+  	  /*java.net.URL imgUrl1 = getClass().getResource(backgroundName);
   	  *ImageIcon gridBackground = new ImageIcon(imgUrl1);
   	  *gridBackground.paintIcon(this,g,0,0);
   	  */
@@ -44,16 +31,29 @@ public class GameGrid extends JComponent
   	  		grid[i][k] = t;
   	  	}
   	  }
-  	  int x,y = 0;
+  	  int x = 0,y = 0;
   	  for(int i = 0; i < grid.length; i++)
   	  {
   	  	for(int k = 0; k < grid[0].length;k++)
   	  	{
-  	  		grid[i][k] = new Tile()
+  	  		grid[i][k] = new Tile(x,y,1);
+  	  		x += 50;
   	  	}
-  	  }
+  	  	x = 0;
+  	  	y += 50;
+  	  }	
+  	}
+  	
+    private void makeGrid(Graphics2D g)
+    {
+		for(int i = 0; i < grid.length; i++)
+		{
+			for(int k = 0; k < grid[0].length;k++)
+  	  		{
+  	  			grid[i][k].makeTile(g);
+  	  		}
+		}
   	  
     }
-    
   }
   
