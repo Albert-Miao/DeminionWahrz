22b631e9937c6be863f6a3385f7f77edf0fcc8df
Nathaniel Getachew <natugetachew@gmail.com>
2017-05-30 12:04:42 -0700

    erkwr

---
+++ Grid/GameGrid.java
@@ -0,0 +1,16 @@
+package Grid;
+
+import javax.swing.JComponent;
+
+public class GameGrid extends JComponent
+{
+	private int row;
+	private int col;
+	private int[][] grid;
+	
+	public GameGrid()
+	{
+		
+	}
+	
+}
