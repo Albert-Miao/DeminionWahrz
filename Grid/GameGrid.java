a6aef01a55ce7ab0a1937b7e30a6508f0ea23d79
Nathaniel Getachew <natugetachew@gmail.com>
2017-05-30 12:06:28 -0700

    erefr

--- Grid/GameGrid.java
+++ Grid/GameGrid.java
@@ -1,16 +1,32 @@
 package Grid;
 
 import javax.swing.JComponent;
+import Element.Tile;
+java.awt.*;
 
 public class GameGrid extends JComponent
 {
-	private int row;
-	private int col;
+	private int numRow;
+	private int numCol;
 	private int[][] grid;
 	
-	public GameGrid()
+	public GameGrid(int r, int c)
 	{
-		
+		numRow = r;
+		numCol = c;
+		grid = new int[numRow][numCol];	
+	}
+	public int getRow()
+	{
+		return numRow;
+	}
+	public int getRow()
+	{
+		return numCol;
+	}
+	public void paintComponent(Graphics g)
+	{
+		Graphics2D g2d = (Graphics2D) g;
 	}
 	
 }
