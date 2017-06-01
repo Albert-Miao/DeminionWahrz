feed50e720180bd6ab481d998fbdd85140765ee6
Albert Miao <amiao541769@gmail.com>
2017-05-31 18:14:10 -0700

    Basic manager layout

---
+++ Managers/Manager.java
@@ -0,0 +1,17 @@
+package Managers;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Manager extends JFrame {
+	public Manager(String name) {
+		super(name);
+		setResizable(false);
+	}
+	
+	public void addComponentsToPane(final Container pane) {
+		final JPanel compsToExperiment = new JPanel();
+		compsToExperiment
+	}
+}
