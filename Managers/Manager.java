906dcf4a67c4aade808e9bec78afb5ec1b520fe1
Albert Miao <amiao541769@gmail.com>
2017-05-31 19:39:55 -0700

    Created layout of layout *ba dum tssssss*

--- Managers/Manager.java
+++ Managers/Manager.java
@@ -4,14 +4,108 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Manager extends JFrame {
+public class Manager extends JFrame implements ActionListener{
+	final static int maxGap = 20;
+	
+	JButton deselectButton;
+	JButton moveButton;
+	JButton attackButton;
+	JButton endTurnButton;
+	
+	GridLayout infoLayout = new GridLayout(0, 2);//map, infodisplay
+	GridLayout controlLayout = new GridLayout(0, 4)//deselect, move, attack, endturn buttons
 	public Manager(String name) {
 		super(name);
 		setResizable(false);
 	}
 	
+	public void initButtons(){
+		deselectButton = new JButton("Deselect");
+		deselectButton.setActionCommand("deselect");
+		
+		moveButton = new JButton("Move");
+		moveButton.setActionCommand("move");
+		
+		attackButton = new JButton("Attack");
+		attackButton.setActionCommand("attack");
+		
+		endTurnButton = new JButton("End Turn");
+		endTurnButton.setActionCommand("end turn");
+		
+		deselectButton.addActionListener(this);
+		moveButton.addActionListener(this);
+		attackButton.addActionListener(this);
+		endTurnButton.addActionListener(this);
+	}
+	
 	public void addComponentsToPane(final Container pane) {
-		final JPanel compsToExperiment = new JPanel();
-		compsToExperiment
+		
+		JPanel info = new JPanel();
+		info.setLayout(infoLayout);
+		JPanel controls = new JPanel();
+		controls.setLayout(controlLayout);
+		
+		JButton b = new JButton ("Fake Button");
+		Dimension buttonSize = b.getPreferredSize();
+		controls.setPreferredSize(new Dimension((int)(buttonSize.getWidth() * 4.5) + maxGap,
+				(int)(buttonSize.getHeight() * 1.5) + maxGap));
+		
+		controls.add(deselectButton);
+		controls.add(moveButton);
+		controls.add(attackButton);
+		controls.add(endTurnButton);
+		
+		pane.add(controls, BorderLayout.SOUTH);
+		pane.add(new JSeparator(), BorderLayout.CENTER);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		
+		switch(e.getActionCommand()) {
+		
+			case "deselect":
+				break;
+			
+			case "move":
+				break;
+			
+			case "attack":
+				break;
+			
+			case "end turn":
+				break;
+				
+			default:
+				break;
+		}
+		
+	}
+	
+	private static void createAndShowGUI(){
+		Manager frame = new Manager("DeminionWahrz");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addComponentsToPane(frame.getContentPane());
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args){
+		try{
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (UnsupportedLookAndFeelException ex){
+			ex.printStackTrace();
+		} catch (IllegalAccessException ex){
+			ex.printStackTrace();
+		} catch (InstantiationException ex){
+			ex.printStackTrace();
+		} catch (ClassNotFoundException ex){
+			ex.printStackTrace();
+		}
+		
+		javax.swing.SwingUtilities.invokeLater(new Runnable(){
+			public void run() {
+				createAndShowGUI();
+			}
+		});
 	}
 }
